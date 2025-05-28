ebe9520c515c46051d6e1b528161d4dafab8afeb
ayanjamil <ayanjamil00@gmail.com>
2025-05-28 18:33:45 +0530

    fix: strucure of the tools into corpus_handlers and document_handler

--- hsn_agent/tools/__init__.py
+++ hsn_agent/tools/__init__.py
@@ -1,8 +1,6 @@
 
-from .create_corpus import create_corpus
-from .list_corpora import list_corpora
-
-
+from .corpus_handler.create_corpus import create_corpus 
+from .corpus_handler.list_corpora import list_corpora
 __all__ = [
     "create_corpus",
     "list_corpora",
